d2d2d54cb11e72bc3d8e4fd7c69ec4408a40af1f
Vladyslav Kolodka <vladkolodka@gmail.com>
2017-06-12 00:46:14 +0300

    Some changes

--- Compiler/Modules/SyntaxAnalyzer.cs
+++ Compiler/Modules/SyntaxAnalyzer.cs
@@ -22,79 +22,51 @@ namespace Compiler.Modules
             // 25
             do
             {
+                Console.WriteLine(currentStateIndex);
                 var state = table.ElementAt(currentStateIndex);
 
                 if (IsTokenExpected(table.ElementAt(currentStateIndex), currentTokenIndex))
                 {
-                    if(state.PushToStack.HasValue) stack.Push(state.PushToStack.Value);
+                    if (state.PushToStack.HasValue) stack.Push(state.PushToStack.Value);
+
+                    if (state.TransisionStateNumber == -1 && state.IsPopFromStackRequired == false)
+                    {
+                        Messages.Add(string.Format(Resources.Messages.ParsingSuccessful, _pool.FileName));
+
+                        return true;
+                    }
+
                     if (state.IsAcceptRequired) currentTokenIndex++;
 
                     try
                     {
                         currentStateIndex = state.IsPopFromStackRequired ? stack.Pop() : state.TransisionStateNumber;
-
                     }
                     catch (InvalidOperationException e)
                     {
-                        Console.WriteLine(e);
-                        throw;
+                        // TODO
+                        Errors.Add("Pop from stack");
+                        return false;
                     }
                 }
                 else if (state.IsErrorOccured)
                 {
                     // TODO throw error
                     Errors.Add(
-                        $"Syntax error : {_pool.Tokens[currentTokenIndex].Class}, {_pool.Tokens[currentTokenIndex].Id}, {_pool.Tokens[currentTokenIndex].Value}");
+                        $"Syntax error : {_pool.Tokens[currentTokenIndex].Class}, {_pool.Tokens[currentTokenIndex].Id}, {_pool.Tokens[currentTokenIndex].Value} : {currentTokenIndex}. Expected: {string.Join("\n", state.ExpectedTokens.Select(pair => $"{pair.Key} : {pair.Value}"))}");
                     return false;
                 }
                 else currentStateIndex++;
-            } while (currentStateIndex != 0);
+            } while (currentStateIndex != -1);
 
             Messages.Add("Syntax analyzer: success.");
-            /*            if (!compilationPool.Tokens.Any())
-                            return false;
-
-                        var parsingTable = Parser.ParseTransitionsTable(ParsingTables.MainParsnigTable);
-
-                        var currentToken = 0;
-                        var currentState = parsingTable.ElementAt(0);
-                        var stack = new Stack<int>();
-                        var tokens = compilationPool.Tokens;
-
-
-                        while (currentState.Id != -1)
-                        {
-                            var token = tokens.ElementAt(currentToken);
-
-                            if (token.Equals(currentState.Class, currentState.NumberInClass) ||
-                                token.Class == TokenClass.Identifier && currentState.Class == TokenClass.Identifier ||
-                                token.Class == TokenClass.Literal && currentState.Class == TokenClass.Literal)
-                            {
-                                if (currentState.IsAcceptRequired) currentToken++;
-
-                                if(currentState.PushToStack.HasValue) stack.Push(currentState.PushToStack.Value);
-                                else if (currentState.IsPopFromStackRequired)
-                                {
-                                    currentState = parsingTable.ElementAt(stack.Pop() - 1);
-                                    continue;
-                                }
-
-                                if(currentState.TransisionStateNumber == -1) continue;
-                                currentState = parsingTable.ElementAt(currentState.TransisionStateNumber - 1);
-                            } else if (currentState.IsErrorOccured)
-                            {
-                                // TODO add error message
-                                return false;
-                            }
-                        }
-            */
 
             return true;
         }
 
         private bool IsTokenExpected(ParsingTableState state, int tokenIndex)
         {
-            if (state.IsNullable/* && state.ExpectedTokens.Any()*/) return true;
+            if (state.IsNullable) return true;
 
             var type = _pool.Tokens[tokenIndex].Class == TokenClass.Identifier
                 ? _pool.Identifiers[_pool.Tokens[tokenIndex].Id].Type
